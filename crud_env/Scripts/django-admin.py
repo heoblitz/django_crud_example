5401e85c4e69e2f3e746030fc6528813aa6c980c
wone2287 <wone2287@gmail.com>
2020-01-11 15:07:21 +0900

    delete python env

--- crud_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\wone2\workspace_home\crud_example\crud_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
