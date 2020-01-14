ae9288885d3b9cdc332930aff29aadc918816206
wone2287 <wone2287@gmail.com>
2020-01-14 16:55:32 +0900

    implement Update feature

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -6,5 +6,7 @@ app_name = 'todo_main'
 urlpatterns = [
     path('', views.Todo_main.as_view(), name='Todo_main'),
     path('insert/', views.check_post, name="Todo_main_insert"),
-    path('<int:pk>/detail/', views.Todo_main_detail.as_view(), name="Todo_main_detail")
+    path('<int:pk>/detail/', views.Todo_main_detail.as_view(), name="Todo_main_detail"),
+    path('<int:pk>/update/', views.Todo_main_update.as_view(), name="Todo_main_update"),
+    path('update_save/', views.Todo_main_update.form_vaild, name="Todo_main_update_save"),
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -16,7 +16,22 @@ class Todo_main_detail(generic.DetailView):
     template_name = 'todo_main/todo_main_detail.html'
     context_object_name = 'todo_list'
 
-    
+class Todo_main_update(generic.UpdateView):
+    model = Todo_list
+    fields = ('title', 'content', 'date')
+    template_name = 'todo_main/todo_main_update.html'
+    success_url = '/home/'
+
+    def form_vaild(self, form):
+        form.save()
+        return render(self.request, 'todo_main/todo_main_success.html', {"message": "일정을 업데이트 했습니다"})
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(object=self.object, form=form)
+        return self.render_to_response(context)
 
 def check_post(request):
     template_name = 'todo_main/todo_main_success.html'
